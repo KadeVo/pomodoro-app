fce39cc6e5ad7a1df47e6e38ca021e17c499fc5a
Kadin Vo <kadinvo@gmail.com>
2023-12-14 17:21:07 +1300

    Added timer component

---
+++ components/Timer.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(1500)
+  const [isActive, setIsActive] = useState(false)
+
+  return <Text>The Pomo Timer</Text>
+}
