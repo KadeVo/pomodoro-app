18ef284a36d10839c530573db9d6457ee0402b39
Kadin Vo <kadinvo@gmail.com>
2024-01-05 13:54:06 +1300

    Added firebase config

---
+++ FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzIykkhxEU_7Mb94-ciI-T7zr304wr4d0',
+  authDomain: 'pomodoro-nativeapp.firebaseapp.com',
+  projectId: 'pomodoro-nativeapp',
+  storageBucket: 'pomodoro-nativeapp.appspot.com',
+  messagingSenderId: '821059457728',
+  appId: '1:821059457728:web:3fc398c9578fdc10379325',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
