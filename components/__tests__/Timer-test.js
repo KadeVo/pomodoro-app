567087a43178b4ce0d5c465cbf89f32acf269952
Kadin Vo <kadinvo@gmail.com>
2024-02-20 17:20:46 +1300

    Adjusted test to see if start and reset button disappear

--- components/__tests__/Timer-test.js
+++ components/__tests__/Timer-test.js
@@ -17,8 +17,16 @@ describe('Timer component', () => {
   it('adjusts timer when "Adjust Timer" button is clicked', () => {
     const { getByText, getByPlaceholderText } = render(<Timer />)
     const adjustTimerButton = getByText('Adjust Timer')
+    const startButton = getByText('Start')
+    const resetButton = getByText('Reset')
+
+    expect(startButton).toBeTruthy()
+    expect(resetButton).toBeTruthy()
 
     fireEvent.press(adjustTimerButton)
+
+    expect(queryByText('Start')).toBeNull()
+    expect(queryByText('Reset')).toBeNull()
 
     const input = getByPlaceholderText('Enter duration in seconds')
     fireEvent.changeText(input, '60')
